7d3b9ab13b3a1c75bf79a4eb12e9833e323fc941
Luke Iorio <lukeiorio@Lukes-MBP.home>
2019-01-14 14:49:59 -0500

    Working Filter

--- static/js/app.js
+++ static/js/app.js
@@ -1,231 +1,98 @@
-// from data.js
+function createTable(dataTable) {
+    /*
+    This function will create the table according to the
+    data table input. If data from data.js is the input, 
+    this will output the table with no filters or sorting.
+    */
+
+    // Remove all rows in current table
+    var prevRows = d3.selectAll("td").remove();
+
+    // assign global variable to data to be displayed
+    window.currentData = dataTable;
+
+    // identify tbody as the d3 select for tbody tag
+    var tbody = d3.select("tbody"); 
+
+    // run a function to append table   
+    dataTable.forEach(function(sighting) {
+    // console.log(sighting)
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key,value]) {
+
+            var cell = row.append("td").attr("class", key);
+            // $(this).addClass(key);
+            cell.text(value);
+        });
+    });
+};
+
+function filterData(filteredData) {
+
+    // collect inputs entered
+    var inputDate = d3.select("#datetime").property("value");
+    var inputState = d3.select("#state").property("value");
+    var inputShape = d3.select("#shape").property("value");
+
+    // filter tableData according to inputs
+    // Can also use cases instead of if statements, but this allows for multiple filters
+    if (inputDate != "") {
+        var filteredData = filteredData.filter(sighting => sighting.datetime === inputDate);
+    };
+    if (inputState != "") {
+        var filteredData = filteredData.filter(sighting => sighting.state === inputState);
+    };
+    if (inputShape != "") {
+        var filteredData = filteredData.filter(sighting => sighting.shape === inputShape);
+    };
+
+    return filteredData;
+}
+
+function handleSubmit() {
+
+    // prevent page refresh
+    d3.event.preventDefault();
+
+    // Define lastSort as empty. When a column is sorted, lastSort will change
+    window.lastSort = "";
+
+    // run create table based on output of filter data
+    createTable(filterData(tableData));
+};
+
+// Sort Data function: 
+function sortTable(key) {
+
+    if (lastSort != key) {
+        lastSort = key;
+        sortedData = currentData.slice().sort(function(a,b) {
+            var x = a[key]; var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    }
+    else {
+        lastSort = "";
+        sortedData = currentData.slice().sort(function(a,b) {
+            var x = a[key]; var y = b[key];
+            return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+        });
+    };
+
+    createTable(sortedData);
+};
+
+// When you First open the page:
+// import data from data.js and create table
 var tableData = data;
 
-// get a reference to the table body
-var tableBody = d3.select("tbody");
+// Define lastSort as empty. When a column is sorted, lastSort will change
+window.lastSort = "";
 
-// add each row of data to the table
-tableData.forEach((sighting) => {
-    //get a new row
-    var row = tableBody.append("tr");
+createTable(tableData);
 
-    //add each entry in a separate column
-    Object.entries(sighting).forEach(([key, value]) => {
-        var entry = row.append("td");
-        entry.text(value);
-    });
-});
+// Select button tag for input:
+var enter = d3.select("#filter-btn");
 
-// filtering table
-// select date button
-var dateButton = d3.select("#filter-btn-date");
-
-// create a function to handle selection
-dateButton.on("click", function() {
-
-    //prevent page from refreshing
-    d3.event.preventDefault();
-
-    //select the input element
-    var inputElement = d3.select("#datetime");
-
-    //get the actual input
-    var inputValue = inputElement.property("value");
-
-    //use the input to filter date
-    var ofDate = tableData.filter(function(sighting) {
-        return sighting.datetime === inputValue;
-    });
-
-    //select the table body
-    var tbody = d3.select("tbody");
-
-    //clear the text
-    tbody.text("");
-
-    //render the filtered table
-    ofDate.forEach((sighting) => {
-        var row = tbody.append("tr");
-
-        Object.entries(sighting).forEach(([key, value]) => {
-            var entry = row.append("td");
-            entry.text(value);
-        });
-    });
-});
-
-//filter by city
-var cityButton = d3.select("#filter-btn-city");
-
-// create a function to handle selection
-cityButton.on("click", function() {
-
-    //prevent page from refreshing
-    d3.event.preventDefault();
-
-    //select the input element
-    var inputElement = d3.select("#city");
-
-    //get the actual input
-    var inputValue = inputElement.property("value");
-
-    //use the input to filter date
-    var ofCity = tableData.filter(function(sighting) {
-        return sighting.city === inputValue;
-    });
-
-    //select the table body
-    var tbody = d3.select("tbody");
-
-    //clear the text
-    tbody.text("");
-
-    //render the filtered table
-    ofCity.forEach((sighting) => {
-        var row = tbody.append("tr");
-
-        Object.entries(sighting).forEach(([key, value]) => {
-            var entry = row.append("td");
-            entry.text(value);
-        });
-    });
-});
-
-//filter by state
-var stateButton = d3.select("#filter-btn-state");
-
-// create a function to handle selection
-stateButton.on("click", function() {
-
-    //prevent page from refreshing
-    d3.event.preventDefault();
-
-    //select the input element
-    var inputElement = d3.select("#state");
-
-    //get the actual input
-    var inputValue = inputElement.property("value");
-
-    //use the input to filter date
-    var ofState = tableData.filter(function(sighting) {
-        return sighting.state === inputValue;
-    });
-
-    //select the table body
-    var tbody = d3.select("tbody");
-
-    //clear the text
-    tbody.text("");
-
-    //render the filtered table
-    ofState.forEach((sighting) => {
-        var row = tbody.append("tr");
-
-        Object.entries(sighting).forEach(([key, value]) => {
-            var entry = row.append("td");
-            entry.text(value);
-        });
-    });
-});
-
-//filter by country
-var countryButton = d3.select("#filter-btn-country");
-
-// create a function to handle selection
-countryButton.on("click", function() {
-
-    //prevent page from refreshing
-    d3.event.preventDefault();
-
-    //select the input element
-    var inputElement = d3.select("#country");
-
-    //get the actual input
-    var inputValue = inputElement.property("value");
-
-    //use the input to filter date
-    var ofCountry = tableData.filter(function(sighting) {
-        return sighting.country === inputValue;
-    });
-
-    //select the table body
-    var tbody = d3.select("tbody");
-
-    //clear the text
-    tbody.text("");
-
-    //render the filtered table
-    ofCountry.forEach((sighting) => {
-        var row = tbody.append("tr");
-
-        Object.entries(sighting).forEach(([key, value]) => {
-            var entry = row.append("td");
-            entry.text(value);
-        });
-    });
-});
-
-//filter by shape
-var shapeButton = d3.select("#filter-btn-shape");
-
-// create a function to handle selection
-shapeButton.on("click", function() {
-
-    //prevent page from refreshing
-    d3.event.preventDefault();
-
-    //select the input element
-    var inputElement = d3.select("#shape");
-
-    //get the actual input
-    var inputValue = inputElement.property("value");
-
-    //use the input to filter date
-    var ofShape = tableData.filter(function(sighting) {
-        return sighting.shape === inputValue;
-    });
-
-    //select the table body
-    var tbody = d3.select("tbody");
-
-    //clear the text
-    tbody.text("");
-
-    //render the filtered table
-    ofShape.forEach((sighting) => {
-        var row = tbody.append("tr");
-
-        Object.entries(sighting).forEach(([key, value]) => {
-            var entry = row.append("td");
-            entry.text(value);
-        });
-    });
-});
-
-//button to reset table
-var resetButton = d3.select("#filter-btn-reload");
-
-//reset function
-resetButton.on("click", function() {
-
-    //prevent reload
-    d3.event.preventDefault();
-
-    //select table body
-    var tbody = d3.select("tbody");
-
-    //clear the text
-    tbody.text("");
-
-    //render the original table
-    tableData.forEach((sighting) => {
-        //get a new row
-        var row = tableBody.append("tr");
-    
-        //add each entry in a separate column
-        Object.entries(sighting).forEach(([key, value]) => {
-            var entry = row.append("td");
-            entry.text(value);
-        });
-    });
-});
+// When button is clicked, run handleSubmit
+enter.on("click", handleSubmit);
